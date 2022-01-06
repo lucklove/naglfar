b4599edf9d67094f151f601d3bcb91642bc9bba1
lucklove <gnu.crazier@gmail.com>
2022-01-06 14:18:53 +0800

    Fix middleware bug

--- server/router.go
+++ server/router.go
@@ -24,5 +24,6 @@ func router(s *Server) http.Handler {
 func httpCORSMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		h.ServeHTTP(w, r)
 	})
 }
